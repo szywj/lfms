b22574889f7490181b619a6c216c3a8949135c27
Jason Zheng <uowenz@foxmail.com>
2017-08-28 18:33:02 +0800

    Implemented seq of datatable, template rows and lazy loading.

--- src/app/user/user-table/user-table.component.ts
+++ src/app/user/user-table/user-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/primeng';
 import { CustomerService } from './services/customer.service';
 import { Customer } from '../../interfaces/customer';
 
@@ -8,26 +9,38 @@ import { Customer } from '../../interfaces/customer';
   styleUrls: ['./user-table.component.scss']
 })
 export class UserTableComponent implements OnInit {
+  datasource: Customer[];
   customers: Customer[];
   cols: any[];
+  totalRecords: number;
 
-  constructor( private customerService: CustomerService ) {
+  constructor(private customerService: CustomerService) {
   }
 
   ngOnInit() {
-    this.customerService.getCustomers().then(customers => this.customers = customers);
+    this.customerService.getCustomers().then(datasource => {
+      this.datasource = datasource;
+      this.totalRecords = datasource.length;
+      this.customers = datasource.slice(0, 3);
+    });
 
     this.cols = [
-      {field: 'customerNo', header: 'Customer No' },
-      {field: 'customerType', header: 'Customer Type' },
-      {field: 'customerName', header: 'Customer Name' },
-      {field: 'customerState', header: 'Customer State' },
-      {field: 'customerCreditLimit', header: 'Customer Credit Limit' },
-      {field: 'customerManager', header: 'Customer Manager' },
-      {field: 'customerCreationDate', header: 'Customer Creation Date' },
-      {field: 'customerLastUpdateDate', header: 'Customer Last Update Date' }
-
+      { field: 'customerNo', header: 'Customer No' },
+      { field: 'customerType', header: 'Customer Type' },
+      { field: 'customerName', header: 'Customer Name' },
+      { field: 'customerState', header: 'Customer State' },
+      { field: 'customerCreditLimit', header: 'Customer Credit Limit' },
+      { field: 'customerManager', header: 'Customer Manager' },
+      { field: 'customerCreationDate', header: 'Customer Creation Date' },
+      { field: 'customerLastUpdateDate', header: 'Customer Last Update Date' }
     ];
   }
 
+  loadCustomersLazy(event: LazyLoadEvent) {
+    setTimeout(() => {
+      if (this.datasource) {
+        this.customers = this.datasource.slice(event.first, event.first + event.rows);
+      }
+    }, 300);
+  }
 }
