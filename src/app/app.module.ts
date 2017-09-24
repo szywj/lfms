29ddd0d861c9a56cdabdfa4833a0af8a77aa7fbd
Jason Zheng <uowenz@foxmail.com>
2017-09-24 23:24:26 +0800

    Customer Info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { WorkspaceComponent } from './workspace/workspace.component';
 import { appRoutes } from './app-routing.module';
+// import { ContentHeadComponent } from './shared/content-head/content-head.component';
 // import { MenuModule, MenuItem } from 'primeng/primeng';
 
 
 @NgModule({
   declarations: [
     AppComponent
+    // ContentHeadComponent
   ],
   imports: [
     BrowserModule,
